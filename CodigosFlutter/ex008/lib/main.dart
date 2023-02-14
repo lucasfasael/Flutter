37f40bf38b98c1286bc3e8448714d2e4dc571214
Lucas <lucas_fasael@hotmail.com>
2023-02-14 15:21:09 -0300

    Novo Ex de Treino!

---
+++ CodigosFlutter/ex008/lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+import 'screens/tela_inicial.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  bool opacidade = true;
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InitialScreen(),
+    );
+  }
+}
+
+
+
