2cff6e524b9eb2d74e395c2d4b65e7d0ca9c9ca2
Lucas <lucas_fasael@hotmail.com>
2024-02-15 08:36:55 -0300

    pt3 atividades

--- atividades/lib/data/taskDAO.dart
+++ atividades/lib/data/taskDAO.dart
@@ -1,4 +1,6 @@
 import 'package:batata/components/task.dart';
+import 'package:batata/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tabelaSql = 'CREATE TABLE $_nomeTabela('
@@ -11,8 +13,65 @@ class TaskDao {
   static const String _difficulty = 'dificuldade';
   static const String _image = 'imagem';
 
-  save() async {}
-  Future<List<Task>> findAll() async {}
-  Future<List<Task>> find(String nomeDaTarefa) async {}
-  delete() async {}
+  save(Task tarefa) async {
+    print("Entrou na função save !!");
+    Database bancoDeDados = await getDatabase();
+    Map<String, dynamic> taskMap = toMap(tarefa);
+
+    var exists = await find(tarefa.nome);
+    if (exists.isEmpty) {
+      print("A tarefa não existe !");
+      bancoDeDados.insert(_nomeTabela, taskMap);
+    } else {
+      print("A tarefa já existe !!");
+      bancoDeDados.update(
+        _nomeTabela,
+        taskMap,
+        where: '$_name = ?',
+        whereArgs: [tarefa.nome],
+      );
+    }
+  }
+
+  Future<List<Task>> findAll() async {
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_nomeTabela);
+    print("FindALL : ${toList(result)}");
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    return tarefas;
+  }
+
+  Map<String, dynamic> toMap(Task tarefa) {
+    Map<String, dynamic> mapa = {};
+    mapa[_name] = tarefa.nome;
+    mapa[_image] = tarefa.foto;
+    mapa[_difficulty] = tarefa.dificuldade;
+    return mapa;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    final Database bandoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bandoDeDados
+        .query(_nomeTabela, where: "$_name = '?' ", whereArgs: [nomeDaTarefa]);
+    print("Tarefa encontrada: ${toList(result)}");
+    return toList(result);
+  }
+
+  delete(String nomeDaTarefa) async {
+    final Database bandoDeDados = await getDatabase();
+    bandoDeDados.delete(
+      _nomeTabela,
+      where: "$_name = ?",
+      whereArgs: [nomeDaTarefa],
+    );
+  }
 }
